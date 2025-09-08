57c99a524f2b8e3a2155b50b855da36d7c77f851
Batt866 <25LP6030@nest.edu.mn>
2025-09-08 12:43:48 +0800

    09.08

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,97 @@
-import Image from "next/image";
+import { MovieCard } from "@/components/home/movie-card";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div>
+      <h1 className="mt-10 text-lg">Upcoming </h1>
+      <div className="flex gap-8 mt-[32px]">
+        <MovieCard title="Dear Santa" score={6.9} image="photo1.jpg" />
+        <MovieCard
+          title="How To Train Your Dragon Live Action"
+          score={6.9}
+          image="photo2.jpg"
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <MovieCard title="Alien Romulus" score={6.9} image="photo3.jpg" />
+        <MovieCard title="From the Ashes" score={6.9} image="photo4.jpg" />
+        <MovieCard title="Space Dogg" score={6.9} image="photo5.png" />
+      </div>
+      <div className="flex gap-8 mt-[32px]">
+        <MovieCard title="The Order" score={6.9} image="photo6.jpg" />
+        <MovieCard title="Y2K" score={6.9} image="photo7.jpg" />
+        <MovieCard
+          title="Solo Leveling: ReAwakening"
+          score={6.9}
+          image="photo8.jpg"
+        />
+        <MovieCard title="Get Away" score={6.9} image="photo9.jpg" />
+        <MovieCard
+          title="Sonic the Hedgehog 3"
+          score={6.9}
+          image="photo10.png"
+        />
+      </div>
+      <h1 className="mt-10 text-lg">Popular</h1>
+      <div className="flex gap-8 mt-[32px]">
+        <MovieCard
+          title="The Shawshank Redemption"
+          score={6.9}
+          image="photo11.jpg"
+        />
+        <MovieCard title="The Godfather" score={6.9} image="photo12.jpg" />
+        <MovieCard title="The Dark Knight" score={6.9} image="photo13.jpg" />
+        <MovieCard title="12 Angry Men" score={6.9} image="photo14.jpg" />
+        <MovieCard
+          title="The Lord of the Rings: The  Return of the King"
+          score={6.9}
+          image="photo15.jpg"
+        />
+      </div>
+      <div className="flex gap-8 mt-[32px]">
+        <MovieCard title="Internstellar" score={6.9} image="photo16.png" />
+        <MovieCard title="Se7en" score={6.9} image="photo17.png" />
+        <MovieCard
+          title="It’s a Wonderful life"
+          score={6.9}
+          image="photo18.png"
+        />
+        <MovieCard title="Seven samurai" score={6.9} image="photo19.png" />
+        <MovieCard
+          title="The Silence of the Lambs"
+          score={6.9}
+          image="photo20.png"
+        />
+      </div>
+      <h1 className="mt-10 text-lg">Top Rated </h1>
+      <div className="flex gap-8 mt-[32px]">
+        <MovieCard title="Pulp Fiction" score={6.9} image="photo21.jpg" />
+        <MovieCard
+          title="The Lord of the Rings: Fellowship of the Kings"
+          score={6.9}
+          image="photo22.jpg"
+        />
+        <MovieCard
+          title="The Good, the Bad and the Ugly"
+          score={6.9}
+          image="photo23.png"
+        />
+        <MovieCard title="Forrest Gump" score={6.9} image="photo24.jpg" />
+        <MovieCard title="Fight Club" score={6.9} image="photo25.jpg" />
+      </div>
+      <div className="flex gap-8 mt-[32px]">
+        <MovieCard
+          title="Saving Private Ryan"
+          score={6.9}
+          image="photo26.png"
+        />
+        <MovieCard title="City of God" score={6.9} image="photo27.png" />
+        <MovieCard title="The Green Mile" score={6.9} image="photo28.png" />
+        <MovieCard title="Life is Beautiful" score={6.9} image="photo29.png" />
+        <MovieCard
+          title="Terminator 2: Judgement Day"
+          score={6.9}
+          image="photo30.png"
+        />
+      </div>
     </div>
   );
 }
---
+++ components/home/movie-card.tsx
@@ -0,0 +1,32 @@
+import { FaStar } from "react-icons/fa";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardTitle,
+} from "../ui/card";
+type MovieCardProps = {
+  title: string;
+  image: string;
+  score: number;
+};
+
+export const MovieCard = ({ title, image, score }: MovieCardProps) => {
+  return (
+    <Card className="w-[230px] bg-secondary p-0 overflow-hidden gap-2">
+      <CardContent className="p-0">
+        <img src={image} alt="" className="w-[230px] h-[340px]" />
+      </CardContent>
+      <CardFooter className="flex flex-col items-start p-2 h-25">
+        <CardDescription className="flex ">
+          <FaStar color="#FDE047" /> <span className="text-black">{score}</span>
+          <span className="text-">/10</span>
+        </CardDescription>
+        <CardTitle className="pt-1 text-lg font-normal not-italic">
+          {title}
+        </CardTitle>
+      </CardFooter>
+    </Card>
+  );
+};
